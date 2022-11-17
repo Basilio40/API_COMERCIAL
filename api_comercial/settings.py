468fc692983a51c7e4261f2f56e81f06446db98b
sandrobispo <Kr9zDnciZbhaZ5tWBLkjm3zPRhqj3Vrq+HMLRuPGoYI=>
2022-11-16 23:41:14 -0300

    modificando Settings

--- api_comercial/settings.py
+++ api_comercial/settings.py
@@ -58,7 +58,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'api_comercial.urls'
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 TEMPLATES = [
     {
@@ -131,4 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
